7dcd147b2e9a43d533dc3ac165be2cbcfd09e164
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-01-22 19:30:04 +0300

    6kyu-Data-reverse

---
+++ 6kyu-Data-reverse/index.js
@@ -0,0 +1,37 @@
+/*
+Description:
+A stream of data is received and needs to be reversed.
+Each segment is 8 bits long, meaning the order of these segments needs to be reversed, 
+for example:
+11111111  00000000  00001111  10101010
+ (byte1)   (byte2)   (byte3)   (byte4)
+ should become:
+ 10101010  00001111  00000000  11111111
+ (byte4)   (byte3)   (byte2)   (byte1)
+ The total number of bits will always be a multiple of 8.
+The data is given in an array as such:
+[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+*/
+
+const data1 = [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0]
+
+// мое решение
+function dataReverse(data) {
+	let result = []
+	for (let i = 0; i < data.length; i += 8) {
+		result.unshift(...data.slice(i, i + 8))
+	}
+	return result;
+}
+
+dataReverse(data1);
+
+// push работает быстрее
+// можно читать исходный массив с конца и пушить в результирующий
+// const dataReverse = data => {
+// 	const result = [];
+// 	while(data.length){
+// 		result.push(...data.splice(-8))
+// 	}
+// 	return result;
+// }
