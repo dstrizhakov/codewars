b557c8161d89b8899f08fe3d3bb178bbf2a24aa7
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-01-24 20:51:18 +0300

    add clever variant

--- 6kyu-N-th-Fibonacci/index.js
+++ 6kyu-N-th-Fibonacci/index.js
@@ -10,7 +10,19 @@ For reference, the first two numbers in the Fibonacci sequence are 0 and 1,
 and each subsequent number is the sum of the previous two.
 */
 
+//мое решение согласно формуле Бине
 function nthFibo(n) {
 	let fibo = ((((1 + Math.sqrt(5)) / 2) ** (n - 1)) - (((1 - Math.sqrt(5)) / 2) ** (n - 1))) / Math.sqrt(5)
 	return Math.trunc(fibo)
 }
+
+// Самое умное решение:
+// function nthFibo(n) {
+// 	let [prev, curr] = [0, 1];
+// 	for (let i = 1; i < n; i++) [prev, curr] = [curr, prev + curr];
+// 	return prev;
+// }
+// Рекурсивное решение:
+// function nthFibo(n) {
+//   return n < 2 ? 0 : n == 2 ? 1 : nthFibo(n-1) + nthFibo(n-2);
+// }
