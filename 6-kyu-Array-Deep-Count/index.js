ce28463eb75dbe62e89d6b98bb5fc17130aebefd
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-03-01 13:08:54 +0300

    6 kyu Length of missing array

--- 6-kyu-Array-Deep-Count/index.js
+++ 6-kyu-Array-Deep-Count/index.js
@@ -33,3 +33,9 @@ function deepCount(array) {
 //   });
 //   return counter;
 // }
+
+console.log(deepCount([]), 0, "Expected 0");
+console.log(deepCount(["x", "y", ["z"]]), 4, "Expected 4");
+console.log(deepCount([1, 2, [3, 4, [5]]]), 7, "Expected 7");
+console.log(deepCount([[[[[[[[[]]]]]]]]]), 8, "Expected 8");
+console.log(deepCount([1, 2, 3]), 3, "Expected 3");
